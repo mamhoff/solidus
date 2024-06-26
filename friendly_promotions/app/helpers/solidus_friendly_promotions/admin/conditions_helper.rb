3f1c82d1f806d5f94c5e7ea312c1a29d3cbbb424
Martin Meyerhoff <mamhoff@gmail.com>
2024-06-26 18:58:53 +0200

    Manually fix Rubocop offenses

--- friendly_promotions/app/helpers/solidus_friendly_promotions/admin/conditions_helper.rb
+++ friendly_promotions/app/helpers/solidus_friendly_promotions/admin/conditions_helper.rb
@@ -3,9 +3,9 @@
 module SolidusFriendlyPromotions
   module Admin
     module ConditionsHelper
-      def options_for_condition_types(benefit, condition)
+      def options_for_condition_types(benefit, selected_condition)
         options = benefit.available_conditions.map { |condition| [condition.model_name.human, condition.name] }
-        options_for_select(options, condition&.type.to_s)
+        options_for_select(options, selected_condition&.type.to_s)
       end
     end
   end
